bea361f7faff6a7c521b99d00982511ca89396eb
Eric Martinez <125086504+emart126@users.noreply.github.com>
2026-05-19 19:58:09 -0700

    Attempting to resolve hiding armor toggle issues

--- scripts/customArmorModule.lua
+++ scripts/customArmorModule.lua
@@ -110,8 +110,6 @@ local wynncraftArmor = {
 
 local wynncraftArmorPathLayer1 = "minecraft:textures/entity/equipment/humanoid/%s.png"
 local wynncraftArmorPathLayer2 = "minecraft:textures/entity/equipment/humanoid_leggings/%s.png"
-local oldWynncraftArmorPathLayer1 = "minecraft:textures/models/armor/%s_layer_1.png"
-local oldWwynncraftArmorPathLayer2 = "minecraft:textures/models/armor/%s_layer_2.png"
 
 for material, materialItem in pairs(wynncraftArmor) do
     kattArmor.Materials[material]
@@ -121,34 +119,60 @@ end
 
 kattArmor.Materials.pale_leather:setDefaultColor(0xA06540)
 
--- -- Wynncraft Hide/Unhide armour --
--- function events.render(delta, context)
---     -- Handle Helmet/Hat visibility
---     -- if (string.find(player:getItem(6).id, "helmet") ~= nil) then
---         vanilla_model.ARMOR:setVisible(false)
---     -- else
---         -- vanilla_model.ARMOR:setVisible(true)
---     -- end
+-- Wynncraft Hide/Unhide Armor --
+OldHelmetUnhidden = false
+OldChestplateUnhidden = false
+OldLeggingsUnhidden = false
+OldBootsUnhidden = false
+function events.render(delta, context)
 
---     -- Helmet
---     kattArmor.Armor.PModel.Upper.head.Helmet:setVisible(not HelmetUnhidden)
---     kattArmor.Armor.PModel.Upper.head.HelmetHat:setVisible(not HelmetUnhidden)
+    Changed = (OldHelmetUnhidden ~= HelmetUnhidden)
+            or (OldChestplateUnhidden ~= ChestplateUnhidden)
+            or (OldLeggingsUnhidden ~= LeggingsUnhidden)
+            or (OldBootsUnhidden ~= BootsUnhidden)
+    if (Changed) then
+        -- print("Changed")
+        kattArmor.forceUpdate()
+    end
 
---     -- Chestplate
---     kattArmor.Armor.PModel.Upper.body.Chestplate:setVisible(not ChestplateUnhidden)
---     kattArmor.Armor.PModel.Upper.body.Arms.Arm_R.RightArmArmor:setVisible(not ChestplateUnhidden)
---     kattArmor.Armor.PModel.Upper.body.Arms.Arm_R.Elbow_R.RightLimbArmor:setVisible(not ChestplateUnhidden)
---     kattArmor.Armor.PModel.Upper.body.Arms.Arm_L.LeftArmArmor:setVisible(not ChestplateUnhidden)
---     kattArmor.Armor.PModel.Upper.body.Arms.Arm_L.Elbow_L.LeftLimbArmor:setVisible(not ChestplateUnhidden)
+    -- Handle Helmet/Hat visibility
+    -- if (string.find(player:getItem(6).id, "helmet") ~= nil) then
+        vanilla_model.ARMOR:setVisible(false)
+    -- else
+        -- vanilla_model.ARMOR:setVisible(true)
+    -- end
+    -- print(HelmetUnhidden, ChestplateUnhidden, LeggingsUnhidden, BootsUnhidden)
 
---     -- Leggings
---     kattArmor.Armor.PModel.Upper.body.Belt:setVisible(not LeggingsUnhidden)
---     kattArmor.Armor.PModel.Lower.Leg_R.RightLeggingsArmor:setVisible(not LeggingsUnhidden)
---     kattArmor.Armor.PModel.Lower.Leg_R.Knee_R.RightAnkleArmor:setVisible(not LeggingsUnhidden)
---     kattArmor.Armor.PModel.Lower.Leg_L.LeftLeggingsArmor:setVisible(not LeggingsUnhidden)
---     kattArmor.Armor.PModel.Lower.Leg_L.Knee_L.LeftAnkleArmor:setVisible(not LeggingsUnhidden)
 
---     -- Boots
---     kattArmor.Armor.PModel.Lower.Leg_R.Knee_R.RightBootArmor:setVisible(not BootsUnhidden)
---     kattArmor.Armor.PModel.Lower.Leg_L.Knee_L.LeftBootArmor:setVisible(not BootsUnhidden)
--- end
+    -- -- Helmet
+    -- ModelHead.Helmet:setVisible(not HelmetUnhidden)
+    -- ModelHead.HelmetHat:setVisible(not HelmetUnhidden)
+    -- ModelHead.HelmetLeather:setVisible(not HelmetUnhidden)
+    -- ModelHead.HelmetHatLeather:setVisible(not HelmetUnhidden)
+    -- ModelHead.HelmetTrim:setVisible(not HelmetUnhidden)
+    -- ModelHead.HelmetHatTrim:setVisible(not HelmetUnhidden)
+
+    -- -- Chestplate
+    -- ModelMainBody.body.Chestplate:setVisible(not ChestplateUnhidden)
+    -- ModelMainBody.body.Chestplate:setVisible(not ChestplateUnhidden)
+    -- ModelMainBody.body.Arms.Arm_R.RightArmArmor:setVisible(not ChestplateUnhidden)
+    -- ModelMainBody.body.Arms.Arm_R.Elbow_R.RightLimbArmor:setVisible(not ChestplateUnhidden)
+    -- ModelMainBody.body.Arms.Arm_L.LeftArmArmor:setVisible(not ChestplateUnhidden)
+    -- ModelMainBody.body.Arms.Arm_L.Elbow_L.LeftLimbArmor:setVisible(not ChestplateUnhidden)
+
+    -- -- Leggings
+    -- kattArmor.Armor.PModel.Upper.body.Belt:setVisible(not LeggingsUnhidden)
+    -- kattArmor.Armor.PModel.Lower.Leg_R.RightLeggingsArmor:setVisible(not LeggingsUnhidden)
+    -- kattArmor.Armor.PModel.Lower.Leg_R.Knee_R.RightAnkleArmor:setVisible(not LeggingsUnhidden)
+    -- kattArmor.Armor.PModel.Lower.Leg_L.LeftLeggingsArmor:setVisible(not LeggingsUnhidden)
+    -- kattArmor.Armor.PModel.Lower.Leg_L.Knee_L.LeftAnkleArmor:setVisible(not LeggingsUnhidden)
+
+    -- -- Boots
+    -- kattArmor.Armor.PModel.Lower.Leg_R.Knee_R.RightBootArmor:setVisible(not BootsUnhidden)
+    -- kattArmor.Armor.PModel.Lower.Leg_L.Knee_L.LeftBootArmor:setVisible(not BootsUnhidden)
+
+    OldHelmetUnhidden = HelmetUnhidden
+    OldChestplateUnhidden = ChestplateUnhidden
+    OldLeggingsUnhidden = LeggingsUnhidden
+    OldBootsUnhidden = BootsUnhidden
+end
